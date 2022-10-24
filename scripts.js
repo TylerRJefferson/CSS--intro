ed68d83fe9f3157fd14d82a94ad32e61549827bb
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-24 15:44:06 -0400

    boxes, padding, margins, and more

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const family = [{
+  dad: true,
+  name:"mike",
+  job: "sales"
+},
+{ mom: true,
+  name: "holly",
+  job: "sales"
+}]
+console.log("hey there, meet my parents", family)
+
+document.getElementById("contact").style.color = 'purple'
+
+const handleColorChange = () => {
+console.log('changing color')
+}
+
+const handleAlert = () => {
+  alert('Alerting the class')
+}
